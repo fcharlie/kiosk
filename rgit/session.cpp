09e62f11aca848f11a888bd96c10ae019140485e
Force Charlie <charlieio@outlook.com>
2018-08-19 22:36:20 +0800

    update

--- rgit/session.cpp
+++ rgit/session.cpp
@@ -4,11 +4,143 @@
 
 ///
 
+constexpr const auto GIT_PROTOCOL = L"GIT_PROTOCOL";
+
+inline bool EndsWith(std::wstring_view s, std::wstring_view suffix) {
+  if (s.size() < suffix.size()) {
+    return false;
+  }
+  return s.compare(s.size() - suffix.size(), suffix.size(), suffix.data()) == 0;
+}
+
+void renderNotFond(net::http_request msg) {
+  /// 404
+  msg.reply(404, "Not Found");
+}
+
+std::optional<std::wstring> HttpSession::PathRepoExist(const std::wstring &rp) {
+  // TODO get path
+  auto np = context->Root() + L"\\" + rp;
+  auto attr = GetFileAttributesW(np.c_str());
+  if (attr != INVALID_FILE_ATTRIBUTES &&
+      (attr & FILE_ATTRIBUTE_DIRECTORY) != 0) {
+    return std::make_optional<std::wstring>(np);
+  }
+  if (!EndsWith(np, L".git")) {
+    np.append(L".git");
+  }
+  attr = GetFileAttributesW(np.c_str());
+  if (attr != INVALID_FILE_ATTRIBUTES &&
+      (attr & FILE_ATTRIBUTE_DIRECTORY) != 0) {
+    return std::make_optional<std::wstring>(np);
+  }
+  return std::nullopt;
+}
+
+/// router
+
 void HttpSession::HttpHandlePost(HttpContext *ctx, net::http_request msg) {
   auto session = std::make_shared<HttpSession>(ctx, msg);
+  session->HandleServiceRpc();
 }
+
 void HttpSession::HttpHandleGet(HttpContext *ctx, net::http_request msg) {
   auto session = std::make_shared<HttpSession>(ctx, msg);
-  wprintf(L"%s\n", msg.absolute_uri().path().c_str());
-  msg.reply(200,"Hello world");
+  session->HandleServiceRefs();
+}
+
+inline const wchar_t *servicecmd(const std::wstring &s) {
+  if (s == L"git-upload-pack") {
+    return L"upload-pack";
+  }
+  if (s == L"git-receive-pack") {
+    return L"receive-pack";
+  }
+  return nullptr;
+}
+
+void HttpSession::HandleServiceRefs() {
+  auto path = net::http::uri::decode(msg.relative_uri().path);
+  if (!EndsWith(path, L"/info/refs")) {
+    msg.reply(404, "Not Found");
+    return;
+  }
+  path.resize(path.size() - sizeof("/info/refs") - 1);
+  auto query = net::uri::split_query(msg.relative_uri().query());
+  auto it = query.find(L"service");
+  if (it == query.end()) {
+    msg.reply(404, "Not Found");
+    return;
+  }
+  const wchar_t *cmd = servicecmd(it->second);
+  if (cmd == nullptr) {
+    msg.reply(400, "Bad Request");
+    return;
+  }
+  auto opath = PathRepoExist(path);
+  if (!opath) {
+    msg.reply(404, "Not Found");
+    return;
+  }
+  std::wstring version;
+  auto it = msg.headers().find(GIT_PROTOCOL);
+  if (it != msg.headers().end()) {
+    version = it->second;
+  }
+  if (version.empty()) {
+    ////
+  }
+  ///
+  //////////////
+}
+void HttpSession::HandleServiceRpc() {
+  auto path = net::http::uri::decode(msg.relative_uri().path);
+  auto np = path.rfind(L'/');
+  if (np == std::wstring::npos) {
+    msg.reply(net::status_codes::BadRequest, "Unsupport url");
+    return;
+  }
+  auto cmd = servicecmd(path.substr(np + 1));
+  if (cmd == nullptr) {
+    msg.reply(404, "Not Found");
+    return;
+  }
+  path.resize(np);
+  auto opath = PathRepoExist(path);
+  if (!opath) {
+    msg.reply(404, "Not Found");
+    return;
+  }
+  std::wstring version;
+  auto it = msg.headers().find(GIT_PROTOCOL);
+  if (it != msg.headers().end()) {
+    version = it->second;
+  }
+}
+
+void HttpSession::WriteToStdin() {
+  auto self(shared_from_this());
+  msg.body().read(target, 4096).then([self, this](size_t count) {
+    ///
+    // auto s(shared_from_this());
+    // return target.putn_nocopy(buffer, count).then([self, this]() {
+
+    //});
+  });
+}
+
+void HttpSession::CopyFromStdout(const net::error_code &ec, size_t bytes) {
+  if (ec) {
+    return;
+  }
+  auto self(shared_from_this());
+  buf.putn_nocopy(buffer, bytes).then([this, self, bytes](size_t p) {
+    if (bytes != p) {
+      return;
+    }
+    process->async_read(net::buffer(buffer),
+                        std::bind(&HttpSession::CopyFromStdout, shared_from_this(),
+                                  std::placeholders::_1,
+                                  std::placeholders::_2));
+  });
 }
--- rgit/session.hpp
+++ rgit/session.hpp
@@ -3,8 +3,11 @@
 #pragma once
 #include "pch.h"
 #include <memory>
+#include <optional>
 #include <cpprest/http_listener.h>
 #include <cpprest/details/http_helpers.h>
+#include <cpprest/filestream.h>
+#include <cpprest/producerconsumerstream.h>
 #include "process.hpp"
 #include "context.hpp"
 
@@ -13,8 +16,14 @@ using namespace web;
 using namespace http;
 using namespace utility;
 using namespace client;
+using namespace concurrency;
 using namespace http::experimental::listener;
 } // namespace net
+
+class AsyncInput {
+public:
+private:
+};
 
 class HttpSession : public std::enable_shared_from_this<HttpSession> {
 public:
@@ -24,8 +33,14 @@ public:
       : context(ctx), msg(msg_) {
     //
   }
+  std::optional<std::wstring> PathRepoExist(const std::wstring &rp);
+
   static void HttpHandlePost(HttpContext *ctx, net::http_request msg);
   static void HttpHandleGet(HttpContext *ctx, net::http_request msg);
+  void HandleServiceRefs();
+  void HandleServiceRpc();
+  void CopyFromStdout(const net::error_code &ec, size_t byres);
+  void WriteToStdin();
 
 private:
   HttpContext *context;
@@ -34,6 +49,9 @@ private:
   std::unique_ptr<web::http::details::compression::stream_decompressor>
       decompressor_;
   std::shared_ptr<HttpProcess> process;
+  net::streams::producer_consumer_buffer<uint8_t> buf;
+  net::streams::streambuf<uint8_t> target;
+  uint8_t buffer[8192];
 };
 
 #endif
